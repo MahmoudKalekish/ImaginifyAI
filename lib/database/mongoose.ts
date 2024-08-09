9b2057f6ad352d82f835697d0807204c58291dbc
mahmoud-kalekish <mahmoud.kalekish@joinin2.com>
2024-08-09 20:10:40 +0300

    fix

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,41 +1,33 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL!;
 
-if (!MONGODB_URL) {
-  throw new Error('Missing MONGODB_URL environment variable');
-}
-
-interface MongooseConnection {
+interface MongooseConn {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+let cached: MongooseConn = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
-export const connectToDatabase = async (): Promise<Mongoose> => {
+export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  try {
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-      dbName: 'imaginify',
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "imaginify",
       bufferCommands: false,
+      connectTimeoutMS: 30000,
     });
 
-    cached.conn = await cached.promise;
-    return cached.conn;
-  } catch (error) {
-    cached.promise = null; // Reset the promise if connection fails
+  cached.conn = await cached.promise;
 
-    // Type narrowing
-    if (error instanceof Error) {
-      throw new Error(`MongoDB connection failed: ${error.message}`);
-    } else {
-      throw new Error('MongoDB connection failed: An unknown error occurred');
-    }
-  }
+  return cached.conn;
 };
